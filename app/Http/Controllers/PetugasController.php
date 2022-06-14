0236ca258f1c7b2a99b7247f66c478d53f6031cc
mil3ni4l <mil3ni4l@yandex.com>
2022-06-14 17:20:13 +0700

    Petugas Created

--- app/Http/Controllers/PetugasController.php
+++ app/Http/Controllers/PetugasController.php
@@ -9,7 +9,7 @@ use App\Talenta;
 
 use Carbon\Carbon;
 use Session;
-  
+
 use Illuminate\Support\Facades\Redirect;
 use Auth;
 use DB;
@@ -36,41 +36,39 @@ class PetugasController extends Controller
 
     public function index()
     {
-                //Akses Dari Luar 
-                if(Auth::user() == '') {
-                    Session::flash('message', 'Oopss..', 'Anda dilarang masuk ke area ini.');
-                    Session::flash('message_type', 'danger');
-                    return redirect()->to('login');
-                } 
-                //AKUN BELUM TERDAFTAR DI TABEL PETUGAS
-                if(Auth::user()->petugas == null) 
-                {
-                    Session::flash('message', 'Anda Belum Ditambahkan Sebagai Petugas !');
-                    Session::flash('message_type', 'danger');
-                    return redirect()->to('/home');
-                } 
+        //Akses Dari Luar 
+        if (Auth::user() == '') {
+            Session::flash('message', 'Oopss..', 'Anda dilarang masuk ke area ini.');
+            Session::flash('message_type', 'danger');
+            return redirect()->to('login');
+        }
+        //AKUN BELUM TERDAFTAR DI TABEL PETUGAS
+        if (Auth::user()->petugas == null) {
+            Session::flash('message', 'Anda Belum Ditambahkan Sebagai Petugas !');
+            Session::flash('message_type', 'danger');
+            return redirect()->to('/home');
+        }
 
         //Selain admin dilarang akses 
-        if(Auth::user()->level == 'bendahara') {
-                Session::flash('message', 'Oopss..', 'Anda dilarang masuk ke area ini.');
-                Session::flash('message_type', 'danger');
+        if (Auth::user()->level == 'bendahara') {
+            Session::flash('message', 'Oopss..', 'Anda dilarang masuk ke area ini.');
+            Session::flash('message_type', 'danger');
             return redirect()->to('/home');
-        } 
+        }
 
-        $petugass = Petugas::orderBy('updated_at','desc')->get();
-        return view('petugas.index',array('petugas' => $petugass));
-  
+        $petugass = Petugas::orderBy('updated_at', 'desc')->get();
+        return view('petugas.index', array('petugas' => $petugass));
     }
 
 
     public function create()
     {
         //Selain admin dilarang akses 
-        if(Auth::user()->level == 'bendahara') {
-                Session::flash('message', 'Oopss..', 'Anda dilarang masuk ke area ini.');
-                Session::flash('message_type', 'danger');
+        if (Auth::user()->level == 'bendahara') {
+            Session::flash('message', 'Oopss..', 'Anda dilarang masuk ke area ini.');
+            Session::flash('message_type', 'danger');
             return redirect()->to('/home');
-        } 
+        }
 
         //MENGITUNG KODE ANGGOTA SECARA OTOMATIS
         $getRow = Petugas::orderBy('id', 'DESC')->get();
@@ -79,24 +77,23 @@ class PetugasController extends Controller
         $kode = "NIP-01";
         if ($rowCount > 0) {
             if ($lastId->id < 9) {
-                    $kode = "NIP-0".''.($lastId->id + 1);
+                $kode = "NIP-0" . '' . ($lastId->id + 1);
             } else {
-                    $kode = "NIP-".''.($lastId->id + 1);
+                $kode = "NIP-" . '' . ($lastId->id + 1);
             }
         }
- 
-        $users = User::WhereNotExists(function($query) {
-                        $query->select(DB::raw(1))
-                        ->from('petugas')
-                        ->whereRaw('petugas.user_id = users.id');
-                     })->get();
+
+        $users = User::WhereNotExists(function ($query) {
+            $query->select(DB::raw(1))
+                ->from('petugas')
+                ->whereRaw('petugas.user_id = users.id');
+        })->get();
 
 
         $petugass = Petugas::get();
-        return view('petugas.create', compact('users','kode', 'petugass'));
-
+        return view('petugas.create', compact('users', 'kode', 'petugass'));
     }
-    
+
 
 
     /**
@@ -106,22 +103,21 @@ class PetugasController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
-    {   
+    {
 
-        $count = Petugas::where('kode_petugas',$request->input('kode_petugas'))->count();
+        $count = Petugas::where('kode_petugas', $request->input('kode_petugas'))->count();
 
-        if($count>0){
+        if ($count > 0) {
             Session::flash('message', 'Already exist!');
             Session::flash('message_type', 'danger');
             return redirect()->to('petugas');
         }
-        
+
         Petugas::create($request->all());
 
         Session::flash('message', 'Berhasil ditambahkan!');
         Session::flash('message_type', 'success');
         return redirect()->route('petugas.index');
-
     }
 
     /**
@@ -131,19 +127,18 @@ class PetugasController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function show($id)
-    {   
+    {
         //Selain admin dilarang akses 
-        if(Auth::user()->level == 'bendahara') {
-                Session::flash('message', 'Oopss..', 'Anda dilarang masuk ke area ini.');
-                Session::flash('message_type', 'danger');
+        if (Auth::user()->level == 'bendahara') {
+            Session::flash('message', 'Oopss..', 'Anda dilarang masuk ke area ini.');
+            Session::flash('message_type', 'danger');
             return redirect()->to('/home');
-        } 
+        }
 
         $data = petugas::findOrFail($id);
         Session::flash('message', 'Berhasil ditambahkan!');
         Session::flash('message_type', 'success');
         return view('Petugas.show', compact('data'));
-        
     }
 
     /**
@@ -153,18 +148,18 @@ class PetugasController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
-    {   
-        
-         //Selain admin dilarang akses 
-        if(Auth::user()->level == 'bendahara') {
+    {
+
+        //Selain admin dilarang akses 
+        if (Auth::user()->level == 'bendahara') {
             Session::flash('message', 'Oopss..', 'Anda dilarang masuk ke area ini.');
             Session::flash('message_type', 'danger');
-        return redirect()->to('/home');
-    } 
+            return redirect()->to('/home');
+        }
 
         $users = User::get();
         $data = Petugas::findOrFail($id);
-   
+
         return view('petugas.edit', compact('data', 'users'));
     }
 
@@ -177,20 +172,20 @@ class PetugasController extends Controller
      */
     public function update(Request $request, $id)
     {
-        if(Auth::user()->level == 'bendahara') {
-           // Alert::info('Oopss..', 'Anda dilarang masuk ke area ini.');
+        if (Auth::user()->level == 'bendahara') {
+            // Alert::info('Oopss..', 'Anda dilarang masuk ke area ini.');
             return redirect()->to('/home');
         }
-        
+
         Petugas::find($id)->update($request->all());
-        
-        if($request->file('gambar') == '') {
+
+        if ($request->file('gambar') == '') {
             $gambar = NULL;
         } else {
             $file = $request->file('gambar');
             $dt = Carbon::now();
             $acak  = $file->getClientOriginalExtension();
-            $fileName = rand(11111,99999).'-'.$dt->format('Y-m-d-H-i-s').'.'.$acak; 
+            $fileName = rand(11111, 99999) . '-' . $dt->format('Y-m-d-H-i-s') . '.' . $acak;
             $request->file('gambar')->move("images/petugas", $fileName);
             //$upload_image = $request->myimage->store('petugas');
             $gambar = $fileName;
@@ -208,16 +203,16 @@ class PetugasController extends Controller
      * @return \Illuminate\Http\Response
      */
 
-     
+
     public function destroy($id)
     {
-  
 
-        if(Auth::user()->level == 'bendahara') {
+
+        if (Auth::user()->level == 'bendahara') {
             return redirect()->to('/home');
         }
-        
-        if(Auth::user()->petugas->id != $id) {
+
+        if (Auth::user()->petugas->id != $id) {
             $user_data = Petugas::findOrFail($id);
             $user_data->delete();
             Session::flash('message', 'Berhasil dihapus!');
@@ -259,5 +254,5 @@ class PetugasController extends Controller
     //     }
     //     return redirect()->to('user');
     // }
-    
+
 }
