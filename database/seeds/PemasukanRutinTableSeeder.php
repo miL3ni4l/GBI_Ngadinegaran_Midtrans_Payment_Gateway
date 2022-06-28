a5fc1657aed0145b887b87a84359701723e6e4f5
mil3ni4l <mil3ni4l@yandex.com>
2022-06-28 10:19:04 +0700

    push

--- database/seeds/PemasukanRutinTableSeeder.php
+++ database/seeds/PemasukanRutinTableSeeder.php
@@ -63,6 +63,32 @@ class PemasukanRutinTableSeeder extends Seeder
                 'tanggal'      => \Carbon\Carbon::now(),
                 'created_at'      => \Carbon\Carbon::now(),
                 'updated_at'      => \Carbon\Carbon::now()
+            ],
+            [
+                'id'  			=> 5,
+                'kode_pemasukan_rutin' 	=> 'TR0001',
+                'ibadah_id'			=> 2,
+                'kategori_id'			=> 1,
+                'kas_id'			=> 2,
+                'nama_pengguna'			=> 1,
+                'status'			=> '1',
+                'nominal'			=> '3000000',
+                'tanggal'      => \Carbon\Carbon::now(),
+                'created_at'      => \Carbon\Carbon::now(),
+                'updated_at'      => \Carbon\Carbon::now()
+            ],
+            [
+                'id'  			=> 6,
+                'kode_pemasukan_rutin' 	=> 'TR0001',
+                'ibadah_id'			=> 2,
+                'kategori_id'			=> 1,
+                'kas_id'			=> 2,
+                'nama_pengguna'			=> 1,
+                'status'			=> '1',
+                'nominal'			=> '1500000',
+                'tanggal'      => \Carbon\Carbon::now(),
+                'created_at'      => \Carbon\Carbon::now(),
+                'updated_at'      => \Carbon\Carbon::now()
             ]
         ]);
     }
